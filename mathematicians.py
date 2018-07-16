cc78c5f1bda1982f1dc42d328da46d7f8ec7210b
Marcus Ackland <mackland@kth.se>
2018-07-16 03:31:16 +0200

    started work, read documentaion for web requests

---
+++ mathematicians.py
@@ -0,0 +1,21 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+def simple_get(url):
+
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+def is_good_response(resp):
+
+def log_error(e):
